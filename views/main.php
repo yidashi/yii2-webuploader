22119982b2786f66d7d8cea78bf9e1e2001a8430
刘俊涛 <liujuntao@corp.1jiajie.com>
2016-07-11 13:56:08 +0800

    bug

--- views/main.php
+++ views/main.php
@@ -1,5 +1,5 @@
 <div id="<?= $boxId ?>">
-    <?php if($image): ?><?= $image ?><?php else: ?><button class="btn btn-primary">选择文件</button><?php endif; ?>
+    <?php if($image): ?><?= $image ?><?php else: ?><button class="btn btn-primary" type="button">选择文件</button><?php endif; ?>
     <div class="uploader-list"></div>
 </div>
 <?= $hiddenInput ?>
